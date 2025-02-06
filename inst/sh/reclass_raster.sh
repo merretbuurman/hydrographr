802567df512fae8f947e6264835d7adaca05d7f0
Merret <merret.buurman@igb-berlin.de>
2025-02-06 18:07:02 +0100

    reclass_raster: Adding log messagest to GRASS session.

--- inst/sh/reclass_raster.sh
+++ inst/sh/reclass_raster.sh
@@ -14,13 +14,27 @@ export BTIFF=$8
 grass -f --gtext --tmp-location  $RASTER   <<'EOF'
 
     # Load raster input file
+    g.message "Input raster file: $RASTER"
+    g.message "Loading input raster file..."
     r.in.gdal --o input=$RASTER  output=raster    --overwrite
+    g.message "Loading input raster file... DONE."
 
     # Reclassify the raster according to the rules
+    # Note: This struggles if the reclass rules text file is very big.
+    g.message "Reclassifying raster file..."
+    g.message "This may use a lot of memory and crash if the input raster is big."
     r.reclass input=raster output=recl_raster rules=$RULES --overwrite
+    g.message "Reclassifying raster file... DONE"
 
     # Export reclassified raster map
+    g.message "Exporting reclassified raster to file with nodata as "$NODATA"..."
     r.out.gdal input=recl_raster output=$OUTPUT type=$TYPE  format=GTiff nodata=$NODATA  --o -f -m -c createopt="COMPRESS=$COMPRESSION,ZLEVEL=$LEVEL,BIGTIFF=$BTIFF"
+    g.message "Exporting reclassified raster to file... DONE."
+
+    # Note: If nodata is not set at all, the following message is printed by GRASS during r.out.gdal:
+    #> Input raster map contains cells with NULL-value (no-data). The value
+    #> -2147483648 will be used to represent no-data values in the input map. You
+    #> can specify a nodata value with the nodata option.
 
 EOF
 
